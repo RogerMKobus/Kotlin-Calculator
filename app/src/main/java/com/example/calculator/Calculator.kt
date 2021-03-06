e2d7e8922bd9dfa24cbe8dbadcfad2b3c7250a64
RogerMKobus <roger.kobus@gmail.com>
2021-03-05 21:20:26 -0300

    Added new options of calculations

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -3,19 +3,69 @@ package com.example.calculator
 import java.util.Scanner
 
 fun main() {
-    sum()
+    println("Wanna calc something real quick?\n Use 1 for addition;\n" +
+            " 2 for subtraction;\n And 3 for multiplication")
+
+    var input = readLine()
+
+    when(input) {
+        "1" -> addition()
+        "2" -> subtraction()
+        "3" -> multiplication()
+        else -> {println("Enter a function number!")
+            main()
+        }
+    }
+
+    println("Something else? \n" +
+            " Type 1 for Yes\n" +
+            " Or anything else for No")
+
+    var repeat = readLine()
+
+    if(repeat == "1"){
+        main()
+    }
 }
 
-fun sum(){
+fun addition (){
     val reader1 = Scanner(System.`in`)
     print("Enter the first number: ")
 
     var num1:Int = reader1.nextInt()
 
     val reader2 = Scanner(System.`in`)
-    println("Enter the second number: ")
+    print("Enter the second number: ")
 
     var num2:Int = reader2.nextInt()
 
     println("$num1 + $num2 = " + (num1 + num2))
+}
+
+fun subtraction(){
+    val reader1 = Scanner(System.`in`)
+    print("Enter the first number: ")
+
+    var num1:Int = reader1.nextInt()
+
+    val reader2 = Scanner(System.`in`)
+    print("Enter the second number: ")
+
+    var num2:Int = reader2.nextInt()
+
+    println("$num1 - $num2 = " + (num1 - num2))
+}
+
+fun multiplication (){
+    val reader1 = Scanner(System.`in`)
+    print("Enter the first number: ")
+
+    var num1:Int = reader1.nextInt()
+
+    val reader2 = Scanner(System.`in`)
+    print("Enter the second number: ")
+
+    var num2:Int = reader2.nextInt()
+
+    println("$num1 * $num2 = " + (num1 * num2))
 }
